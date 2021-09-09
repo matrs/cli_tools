792948d22cab9929f31e92c7289a3a77a3f5f260
matrs <jlmaturanag@gmail.com>
2021-09-09 18:45:33 -0300

    Includes row/genome even if genome doesn't have any (r/t)-RNA

--- making_rnas_table.py
+++ making_rnas_table.py
@@ -1,8 +1,13 @@
 #! /usr/env/ python
 
 from pathlib import Path
+import pandas as pd
+import re
+import Bio
+from Bio import SeqIO
+from Bio.Seq import Seq
+from collections import Counter, defaultdict
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-import pathlib
 
 
 def arg_parser(args):
@@ -30,11 +35,6 @@ def genbank_stats(gbk_file, feat_types=['CDS','rRNA', 'tRNA'], min_len_seq=1400)
         (short ones are still annotated and not marked as pseudo).
         Returns a `defaultdict` where the values of each key are class `Counter`.
     '''
-    import re
-    import Bio
-    from Bio import SeqIO
-    from Bio.Seq import Seq
-    from collections import Counter, defaultdict
 
     parent_dict = defaultdict(list)
     counter = defaultdict()
@@ -45,6 +45,7 @@ def genbank_stats(gbk_file, feat_types=['CDS','rRNA', 'tRNA'], min_len_seq=1400)
                     for k in feat.qualifiers.keys():
                         if k == 'product' and 'pseudo' not in feat.qualifiers.keys():
                             qualifier = feat.qualifiers['product'][0]
+                            print(feat.qualifiers.keys())
                             if (qualifier == '16S ribosomal RNA' and 
                                 len(feat.extract(record.seq)) >= min_len_seq
                                ):
@@ -66,6 +67,9 @@ def genbank_stats(gbk_file, feat_types=['CDS','rRNA', 'tRNA'], min_len_seq=1400)
 #   Create the nested dictionary with the counts of each feat type  
     for k in parent_dict.keys():
         counter[k] = Counter(parent_dict[k])
+    if counter == {}: #'if dict' doesn't work for an empty defaultdict
+        counter['rRNA'] = Counter({'16S ribosomal RNA': 0})
+        counter['tRNA'] = Counter({'tRNA-Gly': 0})
     return counter
 
 
@@ -75,25 +79,22 @@ def create_df(counter_dict, acc_name):
     One DF will be created per dictionary key, where a key is the 
     feature type of interest, then DFs will be merged into one DataFrame
     '''
-    import pandas as pd
     
     inner_dfs = []
     for k in counter_dict.keys():
         #Transpose it to make it "wide" 
         df = pd.DataFrame.from_dict(dict(counter_dict[k].most_common()), 
-                                    orient='index',columns=[acc_name], 
+                                    orient='index', columns=[acc_name], 
                                     dtype='int').T
         inner_dfs.append(df)
-        
+        print('inner_dfs:', inner_dfs)
     # concatenate along the horizontal axis. 
+        print(df.head(5))
     df_concat = pd.concat(inner_dfs, axis=1)
     
     return df_concat
 
 def main(args=None):
-
-    import pandas as pd
-    from pathlib import Path
 
     args = arg_parser(args)
     path = Path(args.input_dir)
@@ -106,9 +107,9 @@ def main(args=None):
         counter = genbank_stats(gbk_file, feat_types=['rRNA', 'tRNA'], min_len_seq=args.min_len)
         # Some genomes may not have any tRNA or rRNA, which will produce
         # and error with create_df()
-        if 'tRNA' not in counter.keys() and 'rRNA' not in counter.keys():
-            print(f"{name} doesn't have any rRNA or tRNA so won't be in the final table")
-            continue
+        # if 'tRNA' not in counter.keys() and 'rRNA' not in counter.keys():
+        #     print(f"{name} doesn't have any rRNA or tRNA so won't be in the final table")
+        #     continue
         counter_df = create_df(counter, name)
         ls_dfs.append(counter_df)
         
