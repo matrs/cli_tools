1b413bd8f15de71a8ee654bccbbf1f0f783316fb
matrs <jlmaturanag@gmail.com>
2021-11-26 15:54:36 -0300

    improve single_copy

--- single_copy_hogs.py
+++ single_copy_hogs.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from collections import defaultdict
 import pandas as pd
 from ete3 import Tree
-import shutil
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
@@ -27,7 +26,7 @@ def arg_parser(args):
     parser.add_argument('hogs_path', 
                         help = 'HOGs sequences path. This sequences should be created by'
                         ' the create_files_for_hogs.py included within the Orthofinder source code')
-    parser.add_argument('tree_path', help = ' SpeciesTree_rooted_node_labels.txt path')
+    parser.add_argument('tree_file', help = ' SpeciesTree_rooted_node_labels.txt path')
     parser.add_argument('-out_dir', default='.',
                         help = 'Directory where the single-copy HOGs list will be written')
     args = parser.parse_args()
@@ -61,17 +60,17 @@ def built_og_dict(nodes_dir, node_to_use):
         og_dict[row['HOG']] = genome_dict
     return og_dict
 
-def select_og_inall(nodes_dir, node_to_use, tree_path):
+def select_og_inall(og_dict, tree_file, node_to_use):
     '''
     Get all the OGs which are present in all the genomes belonging to a 
     specific node (node_to_use).
 
     og_dict: nested dictionary from built_og_dict()
-    tree_path: `SpeciesTree_rooted_node_labels.txt` path.
+    tree_file: `SpeciesTree_rooted_node_labels.txt` path.
     '''
-    og_dict = built_og_dict(nodes_dir, node_to_use)
-    print(f'Total number of OGs which are children of {node_to_use}: {len(og_dict.keys())}')
-    tree = Tree(newick= tree_path, format=1)
+
+    
+    tree = Tree(newick= tree_file, format=1)
     for node in tree.traverse():
         if node.name == node_to_use:
             n = node
@@ -89,11 +88,11 @@ def select_og_inall(nodes_dir, node_to_use, tree_path):
 
     return og_present_inall
 
-def select_single_copy_hogs(og_present_inall):
+def select_single_copy_hogs(hogs_present_inall, og_dict):
 #     i = 0 #for debugging
     hog_single_copy = []
     swt = False
-    for og in og_present_inall:
+    for og in hogs_present_inall:
         # if this loop get completed, the OG has only one gene per genome
         for k,v in og_dict[og].items():
             # Flatten the nested list of genes
@@ -113,8 +112,9 @@ def select_single_copy_hogs(og_present_inall):
 
 def main(args=None):
     args = arg_parser(args)
-    hogs_present_inall = selecting_ogs(args.nodes_dir, args.node_to_use, args.tree_path)
-    hogs_scopy = select_single_copy_hogs(hogs_present_inall)
+    hog_dict = built_og_dict(args.nodes_dir, args.node_to_use)
+    hogs_present_inall = select_og_inall(hog_dict, args.tree_file, args.node_to_use)
+    hogs_scopy = select_single_copy_hogs(hogs_present_inall, hog_dict)
 
     with open(f'{Path(args.out_dir).joinpath(f"single_copy_HOGs_{args.node_to_use}.list")}', 'w') as fh:
         for hog in hogs_scopy:
